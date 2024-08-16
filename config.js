708a84fa84941ae401ab04ef39572b92d362ee36
Akse1588 <155363614+Aksel588@users.noreply.github.com>
2024-08-16 12:47:32 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,121 @@
+window.PorfolioConfig = {
+  name: 'Aksel',
+  jobTitle: 'Junior Back-end developer',
+  jobDescription: ' Junior Back-End Developer skilled in JS, jQuery, Ajax, PHP/OOP, MySQL, and Python. Passionate about creating efficient, scalable solutions. | One year of work experience',
+  skills: [
+    {
+      name: 'HTML',
+      description: ' Fugiat veniam esse consequat quis cillum elit consequat Lorem pariatur ipsum veniam irure incididunt non.'
+    },
+    {
+      name: 'CSS',
+      description: 'Fugiat veniam esse consequat quis cillum elit consequat Lorem pariatur ipsum veniam irure incididunt non.'
+    },
+    {
+      name: 'Python',
+      description: ' Python provides high-level abstractions for common tasks, such as working with files, managing data structures, and handling exceptions, which simplifies code'
+    },
+
+     {
+      name: 'Django',
+      description: '  Django supports various caching mechanisms, allowing you to cache entire views, parts of templates, or querysets to improve performance. This is especially important for high-traffic sites.'
+    },
+    {
+      name: 'JavaScript',
+      description: ' The JavaScript ecosystem, including libraries like jQuery, React, and many others, provides tools and frameworks that simplify complex tasks and reduce the amount of code developers need to write..'
+    },
+    {
+      name: 'jQuery',
+      description: 'jQuery has a vast ecosystem of plugins that extend its functionality. These plugins allow developers to add features like sliders, modals, and form validation with minimal custom code.'
+    },
+
+     {
+      name: 'Ajax',
+      description: 'Ajax has a vast ecosystem of plugins that extend its functionality. These plugins allow developers to add features like sliders, modals, and form validation with minimal custom code.'
+    },
+    {
+      name: 'PHP/OOP',
+      description: ' PHP’s OOP capabilities make it easier to integrate with other systems, APIs, and services. For instance, you can create classes to interface with external libraries or APIs, ensuring a clean separation of concerns and easier integration.'
+    },
+    {
+      name: 'MySQL',
+      description: 'MySQL is trusted by many large enterprises and is used by some of the biggest names in tech, including Facebook, Twitter, and YouTube.'
+    }
+  ],
+  works: [
+    {
+      name: 'TelegramBot Crypto Coins',
+      link: 'https://github.com/Aksel588/TelegramBot_Crypto_Coins.git',
+      description: ' Users can request the current price of various cryptocurrencies, such as Bitcoin, Ethereum, and many others, directly within their Telegram chat. The bot instantly responds with the latest market data, allowing users to stay informed on market movements. -> Python'
+    },
+    {
+      name: 'Personal budget tracker',
+      link: 'https://github.com/Aksel588/Personal-budget-tracker.git',
+      description: 'Esse magna anim incididunt enim sint aute consectetur ipsum id nostrud eu do. Pariatur et anim enim aliquip anim. Laboris labore incididunt labore enim nisi elit excepteur deserunt irure exercitation exercitation. Lorem sit ad laborum sit velit veniam sit nisi nostrud occaecat duis nulla duis.'
+    },
+    {
+      name: 'Weather Now',
+      link: 'https://github.com/Aksel588/Weather-Now.git',
+      description: ' User is currently developing "Weather Now," a Weather App using JavaScript, HTML, and CSS. The project aims to provide a user-friendly interface for displaying real-time weather information and forecasts. -> JavaScript'
+    },
+     {
+      name: 'Program to implement Casino Game',
+      link: 'https://github.com/Aksel588/Program-to-implement-Casino-Game-C-.git',
+      description: 'This program simulates a casino game, such as slots or roulette, using random number generation to determine outcomes. Players can place bets and win or lose based on game results. It includes features for managing bets, displaying results, and tracking player balances, providing an immersive and interactive gaming experience. -> C++'
+    },
+    {
+      name: 'Nous-Shop',
+      link: 'https://github.com/Aksel588/Nous-Shop.git',
+      description: 'Explore Nous-Shop for the latest in fashion, offering a wide range of stylish clothing for men, women, and children. Enjoy a seamless shopping experience with easy navigation and secure payments. Find your perfect look today! -> PHP,JS,MySQL'
+    },
+    {
+      name: 'Generator Char code',
+      link: 'https://github.com/Aksel588/Generator-Char-code.git',
+      description: 'This JavaScript function generates a random uppercase letter by selecting a random character code between 65 and 90 and converting it to a letter. -> JavaScript'
+    }
+  ],
+  interests: [
+    {
+      name: 'Problem Solving ',
+      description: 'I enjoy tackling complex challenges and finding innovative solutions.'
+    },
+    {
+      name: 'Learning New Technologies ',
+      description: ' I am passionate about exploring emerging technologies and expanding my skill set.'
+    },
+    {
+      name: 'Open Source Contribution',
+      description: '  Contributing to open-source projects allows me to collaborate with others and give back to the coding community.'
+    },
+
+    {
+      name: 'Algorithm Design',
+      description: ' Crafting efficient algorithms fascinates me and drives my pursuit of optimization.'
+    },
+    {
+      name: 'Web Development',
+      description: ' Creating dynamic and user-friendly web experiences fuels my creativity and passion for coding.'
+    }
+  ],
+  contacts: {
+    email: 'akseldeveloper8@gmail.com'
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/Aksel588'
+      },
+      {
+        name: 'Linkedin',
+        link: 'https://www.linkedin.com/in/aksel-aghajanyan/'
+      },
+      {
+        name: 'Twitter',
+        link: 'https://x.com/AkselDeveloper'
+      },
+      {
+        name: 'Medium',
+        link: 'https://medium.com/@akseldeveloper'
+      },
+    ]
+  }
+};
